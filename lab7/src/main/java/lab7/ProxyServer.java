1378fa1f86dabf994d54e2ba548e11a89aca35ab
farichase <fbazartinova@mail.ru>
2020-12-24 20:22:40 +0300

    Scripted auto-commit on change (2020-12-24 20:22:39) by gitwatch.sh

--- lab7/src/main/java/lab7/ProxyServer.java
+++ lab7/src/main/java/lab7/ProxyServer.java
@@ -5,9 +5,15 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 public class ProxyServer {
+    public static String ADDR1 = "tcp://localhost:8080";
+    public static String ADDR2 = "tcp://localhost:9000";
     public static void main(String[] args){
         ZContext context = new ZContext();
         ZMQ.Socket clientSocket = context.createSocket(SocketType.ROUTER);
         ZMQ.Socket storageSocket = context.createSocket(SocketType.ROUTER);
+        clientSocket.bind(ADDR1);
+        clientSocket.bind(ADDR2);
+
+        
     }
 }
