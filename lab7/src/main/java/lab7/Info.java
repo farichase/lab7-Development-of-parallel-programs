d63d94f3baf0e56e423e18b665e8be7eade4444e
farichase <fbazartinova@mail.ru>
2020-12-24 22:47:54 +0300

    Scripted auto-commit on change (2020-12-24 22:47:54) by gitwatch.sh

--- lab7/src/main/java/lab7/Info.java
+++ lab7/src/main/java/lab7/Info.java
@@ -1,4 +1,12 @@
 package lab7;
 
+import org.zeromq.ZFrame;
+
 public class Info {
+    private String id;
+    private ZFrame address;
+    private int start;
+    private int end;
+    private long heartBeat;
+    public 
 }
