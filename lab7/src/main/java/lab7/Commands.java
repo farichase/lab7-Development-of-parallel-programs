726a2a816f930a7cf87840da7a414c93a0f5d756
farichase <fbazartinova@mail.ru>
2020-12-24 18:34:18 +0300

    Scripted auto-commit on change (2020-12-24 18:34:18) by gitwatch.sh

--- lab7/src/main/java/lab7/Commands.java
+++ lab7/src/main/java/lab7/Commands.java
@@ -1,11 +1,14 @@
 package lab7;
 
 public class Commands {
+
     public static String setConnectCommand(int start, int end){
         return "Connect" + start + "end";
     }
     public static Commands.CommandType getCommandType(String com){
-        if (GET)
+        if (com.contains("GET")) {
+            return CommandType.GET;
+        }
     }
     public static enum CommandType {
         GET,
