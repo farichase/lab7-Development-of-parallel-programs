5db6f09c102d3a13659a7b3872eb73654d23cde7
farichase <fbazartinova@mail.ru>
2020-12-24 20:19:16 +0300

    Scripted auto-commit on change (2020-12-24 20:19:16) by gitwatch.sh

--- lab7/src/main/java/lab7/ProxyServer.java
+++ lab7/src/main/java/lab7/ProxyServer.java
@@ -1,11 +1,13 @@
 package lab7;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 public class ProxyServer {
     public static void main(String[] args){
         ZContext context = new ZContext();
-        ZMQ.Socket clientSocket = 
+        ZMQ.Socket clientSocket = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket storageSocket = context.createSocket(SocketType.ROUTER);
     }
 }
