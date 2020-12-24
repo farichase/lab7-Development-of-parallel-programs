2f223944668b06e2b702a542c6b8e6e7171132bd
farichase <fbazartinova@mail.ru>
2020-12-24 18:29:32 +0300

    Scripted auto-commit on change (2020-12-24 18:29:32) by gitwatch.sh

--- lab7/src/main/java/lab7/Commands.java
+++ lab7/src/main/java/lab7/Commands.java
@@ -4,4 +4,9 @@ public class Commands {
     public static String setConnectCommand(int start, int end){
         return "Connect" + start + "end";
     }
+    public static enum CommandType {
+        GET,
+        SET,
+        CONNECT;
+    }
 }
