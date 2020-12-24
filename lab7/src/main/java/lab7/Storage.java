63eccb159658493f72a03d0416431cf0c909cc96
farichase <fbazartinova@mail.ru>
2020-12-24 17:02:20 +0300

    Scripted auto-commit on change (2020-12-24 17:02:18) by gitwatch.sh

--- lab7/src/main/java/lab7/Storage.java
+++ lab7/src/main/java/lab7/Storage.java
@@ -1,7 +1,16 @@
 package lab7;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+
 public class Storage {
+    private static String ADDR = "tcp://localhost:9000";
     public static void main(String[] args){
-        
+        ZContext context = new ZContext();
+        ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
+        socket.connect(ADDR);
+
     }
 }
