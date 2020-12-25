d3a2e7104ba9524e4c85515d87377744f90c5353
farichase <fbazartinova@mail.ru>
2020-12-25 23:07:14 +0300

    Scripted auto-commit on change (2020-12-25 23:07:14) by gitwatch.sh

--- lab7/src/main/java/lab7/Commands.java
+++ lab7/src/main/java/lab7/Commands.java
@@ -8,10 +8,10 @@ import java.util.regex.Pattern;
 public class Commands {
     private static final Pattern PUT_P = Pattern.compile("^PUT \\d+ \\d+$", 2);
     private static final Pattern CONNECT_P = Pattern.compile("^CONNECT \\d+ \\d+$", 2);
-    private static final Pattern RESPONSE_P = Pattern.compile("^RESPONSE \\d+ \\d+$", 2);
-    private static final Pattern GET_P = Pattern.compile("^PUT \\d+ \\d+$", 2);
-    private static final Pattern NOTIFY_P = Pattern.compile("^PUT \\d+ \\d+$", 2);
-    private static final Pattern EXIT_P = Pattern.compile("^PUT \\d+ \\d+$", 2);
+    private static final Pattern RESPONSE_P = Pattern.compile("^RESPONSE [a-z\\s\\d]+$", 2);
+    private static final Pattern GET_P = Pattern.compile("^GET \\d+$", 2);
+    private static final Pattern NOTIFY_P = Pattern.compile("^NOTIFY$", 2);
+    private static final Pattern EXIT_P = Pattern.compile("^F$", 2);
     public static String setConnectCommand(int start, int end){
         return "CONNECT" + start + " " + end;
     }
