494fd0bf5185d22d170ee7701f73d143ccb88ef7
farichase <fbazartinova@mail.ru>
2020-12-25 17:17:38 +0300

    Scripted auto-commit on change (2020-12-25 17:17:37) by gitwatch.sh

--- lab7/src/main/java/lab7/Client.java
+++ lab7/src/main/java/lab7/Client.java
@@ -1,7 +1,14 @@
 package lab7;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+
 public class Client {
     public static void main(String[] args){
+        ZContext context = new ZContext();
+        Socket socket = context.createSocket(SocketType.REQ);
+
 
     }
 }
