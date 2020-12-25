8c6f1af885acf1d85c89ada4aa7d66c2bd8b38f4
farichase <fbazartinova@mail.ru>
2020-12-25 16:14:57 +0300

    Scripted auto-commit on change (2020-12-25 16:14:57) by gitwatch.sh

--- lab7/src/main/java/lab7/ProxyServer.java
+++ lab7/src/main/java/lab7/ProxyServer.java
@@ -76,7 +76,7 @@ public class ProxyServer {
                     }
 
                 }
-                if (type == Commands.CommandType.PUT) {
+                if (type == Commands.CommandType.SET) {
                     key = Commands.getKey(com);
                     isKeyValid = sendGetReq(key);
                     if (!isKeyValid){
