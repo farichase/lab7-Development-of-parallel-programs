4cc6c6fc3ed02d9720ebb0ea5dddcfc02f72233d
farichase <fbazartinova@mail.ru>
2020-12-25 17:20:47 +0300

    Scripted auto-commit on change (2020-12-25 17:20:47) by gitwatch.sh

--- lab7/src/main/java/lab7/Client.java
+++ lab7/src/main/java/lab7/Client.java
@@ -5,10 +5,11 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
 public class Client {
+    public static final String CLIENT_ADDRESS = "tcp://localhost:8080";
     public static void main(String[] args){
         ZContext context = new ZContext();
         Socket socket = context.createSocket(SocketType.REQ);
-
+        socket.connect(CLIENT_ADDRESS);
 
     }
 }
